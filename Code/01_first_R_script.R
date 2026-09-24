8384bafff93a9f1174c902882661921deaa198e8
ceciliamagnani16 <cecilia.magnani16@gmail.com>
2026-09-24 12:35:54 +0200

    Update 01_first_R_script.R

--- Code/01_first_R_script.R
+++ Code/01_first_R_script.R
@@ -1,11 +1,11 @@
 # first operation
-2+3
+2 + 3 # R non ha problemi con gli spazi ma tendenzialmente si mettono: 2+3 è uguale a 2 + 3
 
 #first object
-cecilia <- 2+3 # per R <- (assign) è uguale a = 
+cecilia <- 2 + 3 # per R <- (assign) è uguale a = 
 
 #second object
-julius <- 4+6
+julius <- 4 + 6
 
 #additional operation
 cecilia + julius # 2 + 3 + 4 + 6
